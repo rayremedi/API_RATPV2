0cd19ce76a8e714a53d4f90704d2f7d089d805b8
rayremedi <ray.remedi17@laposte.net>
2019-09-09 22:33:59 +0200

    third commit

--- script.js
+++ script.js
@@ -8,11 +8,35 @@ let destinationResult = document.querySelector("#destinationResult");
 let stationResult = document.querySelector("#stationResult");
 let firsttimeResult = document.querySelector("#time");
 let scdtimeResult = document.querySelector("#time2");
+let lineValue = selectLine.value;
+let background = document.querySelector(".result-footer");
+let backgroundColorBis = ["#82C8E6", "#81DC73"];
+let resultRender = document.querySelector(".result");
+let backgroundColor = [
+  "#FFBE00",
+  "#0055C8",
+  "#6E6E00",
+  "#A0006E",
+  "#FF5A00",
+  "#81DC73",
+  "#FF81B4",
+  "#D282BE",
+  "#D2D200",
+  "#DC9600",
+  "#6E491E",
+  "#00643B",
+  "#82C8E6",
+  "#640082"
+];
+
+
 
 function Init() {
   getLines();
   getLineValue();
   searchTime()
+  changeBackground()
+
 }
 
 function getLines() {
@@ -99,12 +123,18 @@ function getLineValue() {
   });
 }
 
+
+
 function searchTime() {
   button.addEventListener("click", () => {
     let lineValue = selectLine.value;
+    const lineImage = document.querySelector("#lineImage");
+    lineImage.setAttribute("src",`./img/${lineValue}.svg`)
     let destinationValue = selectDestination.value;
     let stationValue = selectStation.value;
     // console.log(selectDestination.childNodes.label)
+
+    //Conditions to change the destinationValue to A or R depending of the selected destinations
     if (destinationValue === selectDestination.childNodes.label) {
       console.log("voieA");
       destinationValue = "A"
@@ -113,6 +143,17 @@ function searchTime() {
       destinationValue = "R"
 
     }
+    if (lineValue == "3b") {
+      background.style.backgroundColor = backgroundColorBis[0];
+    } else if (lineValue == "7b") {
+      background.style.backgroundColor = backgroundColorBis[1];
+    }
+    
+    background.style.backgroundColor = backgroundColor[parseInt(lineValue - 1)];
+    resultRender.style.opacity = "1";
+    searchTime();
+    setInterval(searchTime, 30000);
+    
     fetch(`https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/${lineValue}/${stationValue}/${destinationValue}`,{
       method:"get"
     })
@@ -123,6 +164,26 @@ function searchTime() {
       stationResult.innerHTML = "Station : " + selectStation.value;
       firsttimeResult.innerHTML = data.result.schedules[0].message;
       scdtimeResult.innerHTML = data.result.schedules[1].message;
+
+      if (
+        data.result.schedules[0].message === "Train a quai" ||
+        data.result.schedules[0].message === "Train a l'approche" ||
+        data.result.schedules[0].message === "Train retarde"
+      ) {
+        firsttimeResult.classList.add("here");
+      } else {
+        firsttimeResult.classList.remove("here");
+      }
+      if (
+        data.result.schedules[1].message === "Train a quai" ||
+        data.result.schedules[1].message === "Train a l'approche" ||
+        data.result.schedules[1].message === "Train retarde"
+
+      ) {
+        scdtimeResult.classList.add("here");
+      } else {
+        scdtimeResult.classList.remove("here");
+      }
     })
   });
 }
